e129df8182b7794f8479224eb195162435d508d9
Kevin Ward <antiomiae@gmail.com>
2019-06-20 09:02:46 -0700

    .

--- LayerView.py
+++ LayerView.py
@@ -1,6 +1,8 @@
-# This Python file uses the following encoding: utf-8
+# utf-8
 from PySide2 import QtCore
 from PySide2 import QtWidgets
 
-class LayerView:
+
+class LayerView(QtWidgets.QWidget):
     def __init__(self):
+        pass
