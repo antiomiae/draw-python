79be9d4548117ce0fd9e117fd6bdd2d5e48df250
Kevin Ward <antiomiae@gmail.com>
2019-06-21 22:25:09 -0700

    primitive line drawing

--- layer_view.py
+++ layer_view.py
@@ -4,6 +4,8 @@ from PySide2 import QtWidgets
 from PySide2 import QtGui
 from PySide2.QtGui import QPainter, QImage, QPixmap
 
+from dataclasses import dataclass
+
 
 class LayerView(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
@@ -11,28 +13,42 @@ class LayerView(QtWidgets.QWidget):
 
         self.canvas_size = QtCore.QSize(256, 256)
         self.layer = Layer(self.canvas_size)
+        self.tool = None
 
         layout = QtWidgets.QGridLayout()
         scroll_area = QtWidgets.QScrollArea()
         scroll_area.setBackgroundRole(QtGui.QPalette.Dark)
         layout.addWidget(scroll_area, 0, 0)
-
         self.setLayout(layout)
 
         self.image_label = QtWidgets.QLabel()
+        self.image_label.setFrameRect(QtCore.QRect(0, 0, 0, 0))
         self.image_label.setFixedSize(self.canvas_size)
 
-        self.image_label.setPixmap(QPixmap.fromImage(self.layer.image))
-
         scroll_area.setWidget(self.image_label)
+        self.draw_layer()
 
     def mousePressEvent(self, event):
         if not self.tool:
             self.tool = LineTool(self)
         self.tool.on_click(event)
 
+    def line_complete(self, line):
+        p = QPainter(self.layer.image)
+        p.drawLine(QtCore.QLineF(line.start, line.end))
+        p.end()
+        self.tool = None
+        self.draw_layer()
 
+    def draw_layer(self):
+        self.image_label.setPixmap(QPixmap.fromImage(self.layer.image))
+
+
+@dataclass
 class Layer:
+    canvas_size: QtCore.QSize
+    image: QtGui.QImage
+
     def __init__(self, size=QtCore.QSize(128, 128)):
         self.canvas_size = size
         self.image = QImage(self.canvas_size, QImage.Format_ARGB32)
@@ -55,12 +71,15 @@ class LineTool:
 
     def on_click(self, event):
         if not self.start:
-            self.start = event.localPos()
+            start = event.pos()
+            self.start = self.parent.image_label.mapFrom(self.parent, QtCore.QPoint(start.x()-self.parent.image_label.margin(), start.y()-self.parent.image_label.margin()))
+            #breakpoint()
         elif not self.end:
-            self.end = event.localPos()
+            end = event.pos()
+            self.end = self.parent.image_label.mapFrom(self.parent, QtCore.QPoint(end.x(), end.y()))
 
         if self.start and self.end:
             self.line_complete()
 
     def line_complete(self):
-        pass
+        self.parent.line_complete(self)
